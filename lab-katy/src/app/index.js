794a500fc434cac5e38fe05c5710d5fdfc98ddc9
Katy Robinson <katyorob@gmail.com>
2018-03-26 13:32:14 -0700

    app component built

---
+++ lab-katy/src/app/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import React from 'react';
+import {Provider} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom';
+import DashboardContainer from '../dashboard-container';
+import createAppStore from '../../lib/store.js';
+
+const store = createAppStore();
+
+//entire state managed here below. bc the route throws component. the store holds the state of our app. always wrap app in provider//things that you need to access in your application in your provider
+
+class App extends React.Component {
+  componentDidMount() {
+    store.subscribe( () => {
+      console.log('__STATE__', store.getState());
+    });
+
+    store.dispatch({ type: null })
+  }
+
+  render() {
+    return (
+      <section>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Route exact path='/' component={DashboardContainer} />
+          </BrowserRouter>
+        </Provider>
+      </section>
+    )
+  }
+}
+
+export default App;
